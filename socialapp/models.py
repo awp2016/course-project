f0da6402690299997a5cfd5c62cd49521b97f27c
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2016-11-09 10:14:13 +0200

    Add first model

--- socialapp/models.py
+++ socialapp/models.py
@@ -2,4 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Status(models.Model):
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(default="Eau de Web", max_length=50)
