cca62ffc4b3b2552f30c35ebca7becd13b77f81f
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2016-11-16 08:24:12 +0200

    Add status details view

--- socialapp/views.py
+++ socialapp/views.py
@@ -7,3 +7,9 @@ def index(request):
     status_list = models.Status.objects.all()
     return render(request, 'socialapp/index.html',
                   {'status_list': status_list})
+
+
+def status_details(request, pk):
+    status = models.Status.objects.get(pk=pk)
+    context = {'status': status}
+    return render(request, 'socialapp/status_details.html', context)
